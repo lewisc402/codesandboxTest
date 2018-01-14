730e11c067b1ce06a16af14db7036985a9d7effb
lewisc402 <lewisc402@gmail.com>
2018-01-14 13:43:08 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Card } from 'antd';
+
+ReactDOM.render(
+  <div style={{ background: '#ECECEC', padding: '30px' }}>
+    <Card title="Card title" bordered={false} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+  </div>
+, document.getElementById('container'));
+          
